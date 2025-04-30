a6bb0bbdd7a91a744ccf2e603bcfa3ca1c2887de
Esty8321 <m025863821@gemail.com>
2025-04-30 16:40:08 +0300

    adding the missing files

---
+++ client/my-app/src/components/File.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+
+const FileExample = (props) => {
+    const userName = props.userName
+    const contentFile = props.contentFile;
+    const[contentFileReady,setContentFileReady]=useState("")
+    const[loading,setLoading]=useState(true)
+    useEffect(() => {
+
+        const readyContent = contentFile.map(userTask => {
+            // console.log('the name of the task: \n',userTask.task);
+            let sentence =""
+            const line=userTask.questions.map(question => {
+                sentence += `question-${question.text}? \n`
+                const matchAnswer = userTask.answers.find(answer => answer.question === question._id)
+                sentence += `answer-${matchAnswer?.text}\n\n`
+                // console.log("the question and the answer:  \n",sentence);
+                return sentence
+            })
+            return `the name of the task:'${userTask.task}\n\n`+line
+        })
+
+        console.log('the array: ', readyContent);
+        setContentFileReady(readyContent)
+        setLoading(false)
+    }, [])
+    const handleDownload = () => {
+        // const content = 'Hello, this will be saved to a file.';
+        const blob = new Blob([contentFileReady], { type: 'text/plain' });
+        const url = URL.createObjectURL(blob);
+
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = userName; // The name of the file to be downloaded
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+        URL.revokeObjectURL(url); // Clean up
+    };
+
+    return (
+       <>{loading?<>Loading</>:
+       <div>
+            <h1>Download File Example</h1>
+            <button onClick={handleDownload}>Download File</button>
+        </div>}</>
+    );
+}
+
+export default FileExample
